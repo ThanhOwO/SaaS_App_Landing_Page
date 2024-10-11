5f078e33984fe901741c1a1ab152e20e4400ea79
ThanhOwO <thanhlepro123@gmail.com>
2024-10-11 16:29:05 +0700

    Finish feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import Header from './sections/Header'
+import React from "react";
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
+import Feature from "./sections/Feature";
 
 const App = () => {
   return (
-    <main className='overflow-hidden'>
-      <Header/>
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Feature />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
